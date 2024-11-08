1071e978cdecf1d2ce44b17dff13796f66a8d924
Michal Polomski <michal.polomski@o2.pl>
2024-11-08 23:57:59 +0100

    slider to volume

--- lib/src/components/UI/volume.dart
+++ lib/src/components/UI/volume.dart
@@ -1,14 +1,61 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:pianogame/src/config.dart';
 
-class Volume extends RectangleComponent {
+class Volume extends RectangleComponent with DragCallbacks, TapCallbacks{
   double outVolume;
   Volume({this.outVolume=defaultVomule}):super(
     paint: Paint()..color = transparent,
     size: Vector2(sliderWidth, sliderHeight),
     position: Vector2(10*whiteKeyWidth, (keyboardHeight - 3*(gameWidth/2-keyboardWidth/2))/2 - sliderHeight*4/2)
   );
+
+  double volume = 0.5; // Początkowa głośność
+
+  // Wskaźnik pozycji
+  late RectangleComponent _sliderRect;
+  late RectangleComponent _knobRect;
+
+  @override
+  Future<void> onLoad() async {
+    _sliderRect = RectangleComponent(position: Vector2(0, 0), size: Vector2(sliderWidth, sliderHeight));
+    _knobRect = RectangleComponent(paint: (Paint()..color= blackKey), position: Vector2(volume * sliderWidth - 10, -10), size: Vector2(20, 40));
+    _updateSlider();
+    add(_sliderRect);
+    add(_knobRect);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    _updateVolume(event.localPosition.x);
+    FlameAudio.bgm.audioPlayer.setVolume(volume); 
+    super.onTapDown(event);
+  }
+
+  @override
+  void onDragUpdate(DragUpdateEvent event) {
+    _updateVolume(event.localEndPosition.x);
+    FlameAudio.bgm.audioPlayer.setVolume(volume);
+    super.onDragUpdate(event);
+  }
+
+  // Funkcja do aktualizacji głośności na podstawie położenia wskaźnika
+  void _updateVolume(double x) {
+    double relativeX = (x).clamp(0, sliderWidth);
+    volume = relativeX / sliderWidth;
+    _updateSlider();
+  }
+
+  // Aktualizuje pozycje prostokątów (paska i wskaźnika)
+  void _updateSlider() {
+    _knobRect.position.x =volume * sliderWidth - 10;
+    // RectangleComponent _knobRect = Rect.fromLTWH(position.x + volume * sliderWidth - 10, position.y - 10, 20, 40);
+    
+    
+    
+  }
 
 
 
