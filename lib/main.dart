3ec12075c06d9eb241ede82d749e6c471292d014
Michal Polomski <michal.polomski@o2.pl>
2025-01-16 22:53:00 +0100

    always horizontal

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'src/pianogame.dart';
 
 void main() {
   final game = PianoGame();                                  // Modify this line
-  runApp(GameWidget(game: game));
+
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Wymuszenie orientacji horyzontalnej
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((_) {
+    runApp(GameWidget(game: game));
+  });
 }
